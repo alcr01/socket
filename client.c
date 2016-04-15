906e057ed1b12c4a546607a7f0e413cfb2bfba55
Alex <alcr01@outlook.com>
2016-04-14 23:05:50 -0500

    Added files via upload

--- client.c
+++ client.c
@@ -1,66 +1,316 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <time.h>
+#include <sys/ioctl.h>
+
+#define BUFFSIZE 10
+#define WEBSIZE 100
 
 void error(const char *msg)
 {
 	perror(msg);
-	exit(0);
+	exit(1);
 }
 
-int main(int argc, char *argv[])
+// Program 3 Variables
+struct tCPSegment {  
+    short int destPort;                 // 16 bit destination port
+    short int sourcePort;               // 16 bit source port
+    short int reserve;                  // 6 bit reserved section
+    short int flags;                 	// Combined fields of d, e, and f
+    short int window;              		// 16 bit receive window for flow control
+    short int checksum;                 // 16 bit checksum
+    int sequence;                       // 32 bit sequence number
+    int ack;                            // 32 bit acknowledgement number
+    int options;                    	// 32 bit options
+};
+
+int main()
 {
-	int sockfd, portno, n;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
+	 // Variables
+    int createSocket, n, run = 1;
+    struct sockaddr_in servaddr;        // Servers connection
+    struct hostent *server;			
+    socklen_t addrlen;
+    char *website = malloc(WEBSIZE);    
+    int *buffer = (int*) malloc(BUFFSIZE * sizeof(int));    // Buffer where everything will be saved
+	struct tCPSegment t;
+	unsigned short int cksum_arr[12];	// Checksum
+	unsigned int sum = 0, i, cksum;	
+	int s_bit = 0, a_bit = 0;
+	int f_bit = 0;			
 
-	char buffer[256];
-	if(argc < 3) {
-		fprintf(stderr, "usage %s hostname port\n", argv[0]);
-		exit(0);
-	}
-	portno = atoi(argv[2]);
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0) 
-		error("ERROR opening socket");
-	server = gethostbyname(argv[1]);
+	// Assign initial values for the first TCP Segment being sent
+	srand(time(NULL));					// Randomize the random seed
+	t.sequence = random() % BUFFSIZE;	// Random sequence number
+	t.ack = 0;
+	t.destPort = 2200;
+	t.sourcePort = 21334;
+	t.window = 0;
+	t.checksum = 0;
+	t.reserve = 0;
+	t.options = 0;
+	/*
+		// Set 0 for all the flags
+	for(n = 0; n < 6; n++)
+		t.flags[n] = 0;
+	t.flags[4] = 1;						// Sets SYNBIT to 1
+	*/
+	memcpy(cksum_arr, &t, 24); 			//Copying 24 bytes
+	for (i=0;i<12;i++)               	// Compute sum
+		sum = sum + cksum_arr[i];
+
+	cksum = sum >> 16;              	// Fold once
+	sum = sum & 0x0000FFFF; 
+	sum = cksum + sum;
+
+	cksum = sum >> 16;             		// Fold once more
+	sum = sum & 0x0000FFFF;
+	cksum = cksum + sum;
+
+	cksum = 0xFFFF^cksum;
+
+	t.checksum = cksum;
+
+    // Creates the socket and checks if it created successfully
+    if((createSocket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
+        printf("Socket created!\n");
+   
+	// Will get the server for the client to connect to
+    printf("Server CSE Machine: ");
+    scanf("%s", website);
+
+	server = gethostbyname(website);
 	if(server == NULL) {
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
+		error("ERROR, no such host\n");
 	}
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
-	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	bzero((char *) &servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&servaddr.sin_addr.s_addr, server->h_length);
+	servaddr.sin_port = htons(t.destPort);
+
+	if(connect(createSocket, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
 		error("ERROR connecting");
+	for(n = 0; n < sizeof(buffer); n++)
+		buffer[n] = 0;
 
+	// TCP Connection
+	if(run == 1)
+	{
+		buffer[0] = t.sourcePort;	// Source Port
+		buffer[1] = t.destPort;		// Destination Port
+		buffer[2] = t.sequence;		// Sequence Number
+		buffer[3] = t.ack;			// Ack number
+			// SYN AND ACK BITS
+		s_bit = 1;
+		buffer[4] = s_bit;			// SYNBIT
+		buffer[5] = a_bit;			// ACKBIT
+		buffer[6] = t.window;		// Window
+		buffer[7] = t.checksum;		// Checksum
+		buffer[8] = t.options;		// Options
+		run++;						// Second transition ready
 
-	srand(time(NULL));		// Randomize the random seed
-	if(random() % 10 == 7) { //Cancel
-		printf("Canceling tickets");
-		bzero(buffer, 256);
-		fgets(buffer, 255, stdin);
-
-		n = write(sockfd, buffer, strlen(buffer));
+		// Send to the server
+		n = write(createSocket, buffer, sizeof(buffer));
 		if(n < 0) error("ERROR writing to socket");
-		bzero(buffer, 256);
 
-		n = read(sockfd, buffer, 255);
-		if(n < 0) error("ERROR reading from socket");
-		printf("%s\n", buffer);
-
+		// PRINT OUT WHAT THE CLIENT SENDS TO SERVER
+		printf("CLIENT -> SERVER SENT\n");
+		printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+		printf("Seq: %d\n", t.sequence);
+		printf("ACK: %d\n", t.ack);
+		printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+		printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+		printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d\n", s_bit, f_bit, t.window);
+		printf("CHECKSUM: 0x%04X\n", t.checksum);
+		printf("OPTIONS: %d\n DATA: 0\n", t.options);
+		printf("\n\n");
 	}
-	else { // Buy
-		printf("Buying tickets");
+	// Second run
+	if(run == 2)
+	{	
+		n = read(createSocket, buffer, sizeof(buffer));
+		if(n < 0) error("ERROR on reading from socket");
+
+		t.sequence =  t.sequence + 1;	// Sequence number plus 1
+		t.ack = buffer[2];				// Changes the ACK number
+		a_bit = 1;						// Changes the ack bit
+
+		memcpy(cksum_arr, &t, 24); 		//Copying 24 bytes
+		for (i=0;i<12;i++)              // Compute sum
+			sum = sum + cksum_arr[i];
+
+		cksum = sum >> 16;             	// Fold once
+		sum = sum & 0x0000FFFF; 
+		sum = cksum + sum;
+
+		cksum = sum >> 16;             	// Fold once more
+		sum = sum & 0x0000FFFF;
+		cksum = cksum + sum;
+		cksum = 0xFFFF^cksum;
+
+		t.checksum = cksum;
+		run++;
+
+		buffer[2] = t.sequence;
+		buffer[3] = t.ack;
+		buffer[5] = a_bit;
+		buffer[7] = t.checksum;
+
+		// Send to the server
+		n = write(createSocket, buffer, sizeof(buffer));
+		if(n < 0) error("ERROR writing to socket");
+
+		// PRINT OUT WHAT THE CLIENT SENDS TO SERVER
+		printf("\nWHAT CLIENT SENDS TO SERVER\n");
+		printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+		printf("Seq: %d\n", t.sequence);
+		printf("ACK: %d\n", t.ack);
+		printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+		printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+		printf("\tSYN: %d \n\tFINBIT: 0 \n\tWINDOW: %d", s_bit, t.window);
+		printf("CHECKSUM: 0x%04X\n", t.checksum);
+		printf("OPTIONS: %d\n DATA: 0\n", t.options);
+		printf("\n\n");
 	}
 
-	close(sockfd);
+	
+	//Closing TCP connection
+	if(run == 3)
+	{
+		srand(time(NULL));					// Randomize the random seed
+		t.sequence = random() % BUFFSIZE;
+		t.ack = 0;
+		f_bit = 1;
+
+		memcpy(cksum_arr, &t, 24); 		//Copying 24 bytes
+		for (i=0;i<12;i++)              // Compute sum
+			sum = sum + cksum_arr[i];
+
+		cksum = sum >> 16;             	// Fold once
+		sum = sum & 0x0000FFFF; 
+		sum = cksum + sum;
+
+		cksum = sum >> 16;             	// Fold once more
+		sum = sum & 0x0000FFFF;
+		cksum = cksum + sum;
+		cksum = 0xFFFF^cksum;
+
+		t.checksum = cksum;
+		run++;
+
+		buffer[2] = t.sequence;		// Sequence Number
+		buffer[3] = t.ack;			// Ack number
+			// SYN AND ACK BITS
+		f_bit = 1;
+		a_bit = 0;
+		buffer[4] = f_bit;			// FINBIT
+		buffer[5] = a_bit;			// ACKBIT
+		buffer[6] = t.window;		// Window
+		buffer[7] = t.checksum;		// Checksum
+		buffer[8] = t.options;		// Options
+
+		// Send to the server
+		n = write(createSocket, buffer, sizeof(buffer));
+		if(n < 0) error("ERROR writing to socket");
+
+		// PRINT OUT WHAT THE CLIENT SENDS TO SERVER
+		printf("\n\nCLOSING TCP\n");
+		printf("CLIENT -> SERVER SENT\n");
+		printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+		printf("Seq: %d\n", t.sequence);
+		printf("ACK: %d\n", t.ack);
+		printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+		printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+		printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d\n", s_bit, f_bit, t.window);
+		printf("CHECKSUM: 0x%04X\n", t.checksum);
+		printf("OPTIONS: %d\n DATA: 0\n", t.options);
+		printf("\n\n");
+	}
+	if(run == 4)
+	{
+		n = read(createSocket, buffer, sizeof(buffer));
+		if(n < 0) error("ERROR on reading from socket");
+
+		t.sequence = buffer[2];
+		t.ack = buffer[3];
+		f_bit = buffer[4];
+		a_bit = buffer[5];
+		t.checksum = buffer[7];
+
+		// PRINT OUT WHAT THE CLIENT SENDS TO SERVER
+		printf("\n\nCLOSING TCP\n");
+		printf("CLIENT -> SERVER SENT\n");
+		printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+		printf("Seq: %d\n", t.sequence);
+		printf("ACK: %d\n", t.ack);
+		printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+		printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+		printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d\n", s_bit, f_bit, t.window);
+		printf("CHECKSUM: 0x%04X\n", t.checksum);
+		printf("OPTIONS: %d\n DATA: 0\n", t.options);
+		printf("\n\n");
+
+		int *buffer2 = (int*) malloc(BUFFSIZE * sizeof(int));    // Buffer where everything will be saved
+		n = read(createSocket, buffer2, sizeof(buffer2));
+		if(n < 0) error("ERROR on reading from socket");
+		
+		t.sequence = buffer[2];
+		t.ack = buffer[3];
+		f_bit = buffer[4];
+		a_bit = buffer[5];
+		t.checksum = buffer[7];
+
+		// PRINT OUT WHAT THE CLIENT SENDS TO SERVER
+		printf("\n\nCLOSING TCP\n");
+		printf("CLIENT -> SERVER SENT\n");
+		printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+		printf("Seq: %d\n", t.sequence);
+		printf("ACK: %d\n", t.ack);
+		printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+		printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+		printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d\n", s_bit, f_bit, t.window);
+		printf("CHECKSUM: 0x%04X\n", t.checksum);
+		printf("OPTIONS: %d\n DATA: 0\n", t.options);
+		printf("\n\n");
+
+
+		t.ack = t.sequence;
+		t.sequence = t.sequence + 1;
+		a_bit = 1;
+
+		buffer[2] = t.sequence;
+		buffer[3] = t.ack;
+		buffer[5] = a_bit;
+
+		memcpy(cksum_arr, &t, 24); 		//Copying 24 bytes
+		for (i=0;i<12;i++)              // Compute sum
+			sum = sum + cksum_arr[i];
+
+		cksum = sum >> 16;             	// Fold once
+		sum = sum & 0x0000FFFF; 
+		sum = cksum + sum;
+
+		cksum = sum >> 16;             	// Fold once more
+		sum = sum & 0x0000FFFF;
+		cksum = cksum + sum;
+		cksum = 0xFFFF^cksum;
+
+		t.checksum = cksum;
+
+		buffer[7] = t.checksum;
+
+		// Send to the server
+		n = write(createSocket, buffer, sizeof(buffer));
+		if(n < 0) error("ERROR writing to socket");
+	}
+	
+
+	close(createSocket);
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+#define BUFFSIZE 10
 
 void error(const char *msg)
 {
@@ -13,92 +16,258 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-	int sockfd, newsockfd, portno;
-	socklen_t clilen;
-	struct sockaddr_in serv_addr, cli_addr;
+// Program 3 Variables
+struct tCPSegment {  
+    short int destPort;                 // 16 bit destination port
+    short int sourcePort;               // 16 bit source port
+    short int reserve;                  // 6 bit reserved section
+    short int flags;                    // Combined fields of d, e, and f
+    short int window;                   // 16 bit receive window for flow control
+    short int checksum;                 // 16 bit checksum
+    int sequence;                       // 32 bit sequence number
+    int ack;                            // 32 bit acknowledgement number
+    int options;                        // 32 bit options
+};
+
+int main(){
+    // Variables
+    int createSocket, newSocket;
+    int *buffer = (int*) malloc(BUFFSIZE * sizeof(int));    // Buffer where everything will be saved
+    struct sockaddr_in addr;            // Clients connection   
+    struct sockaddr_in servaddr;        // Servers connection
+    socklen_t addrlen;
+    struct tCPSegment t;
+    unsigned short int cksum_arr[12];   // Checksum
+    unsigned int sum = 0, i, cksum; 
+    int s_bit = 0, a_bit = 0, f_bit = 0;
+    int run = 1, n = 0;           
 
 
-	char buffer[256];
-	int ticket[10], 			// 10 tickets sold
-		t_sold[10];				// Checks if the ticket was sold
-	int n, i, t_count = 0;		// Counters
-	int to_cancel;				// Ticket that'll be canceled
+    // Creates the socket and checks if it created successfully
+    if((createSocket = socket(AF_INET, SOCK_STREAM, 0)) > 0)
+        printf("Socket created!\n");
+    
+    // Something to do with sockets and port access (binding)
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(2200);
 
-	/* *************************************************************
-	Adds random numbers (ie the tickets) and adds 0 (ticket not sold)
-	to the int array of ticket and t_sold
-	************************************************************** */
-	srand(time(NULL));			// Randomize the seed
-	for(i = 0; i < 10; i++) {
-		// Adds random numbers to tickets
-		ticket[i] = rand() % 90000 + 10000;
-		t_sold[i] = 0;			// Assigns no tickets sold 
-	}
+        //Binds the port to socket
+    if(bind(createSocket, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+        error("ERROR on binding");
+    printf("Succesfully Bound!\n");
+    // Checks if socket is listening on the port
+    if(listen(createSocket, 10) < 0)
+        error("ERROR on Listening!");
+    printf("Listening...\n");
+    // Checks if the client is able to connect
+    addrlen = sizeof(addr);
+    if((newSocket = accept(createSocket, (struct sockaddr *) &addr, &addrlen)) < 0) 
+        error("ERROR on accept!");
+    printf("Client connected!\n");
 
+    if(run == 1)
+    {
+        n = read(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR reading to socket");
 
-	if (argc < 2) {
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
+        t.sourcePort = 2200;        // Source Port
+        t.destPort = buffer[0];     // Destination Port
+        t.sequence = buffer[2];     // Sequence Number
+        t.ack = buffer[2] + 1;     // Ack number
+            // SYN AND ACK BITS
+        s_bit = buffer[4];          // SYNBIT
+        a_bit = 1;                  // ACKBIT
+        t.window = buffer[6];       // Window
+        t.checksum = buffer[7];     // Checksum
+        t.options = buffer[8];      // Options
+
+        // Runs Checksum
+        memcpy(cksum_arr, &t, 24);          //Copying 24 bytes
+        for (i=0;i<12;i++)                  // Compute sum
+            sum = sum + cksum_arr[i];
+
+        cksum = sum >> 16;                  // Fold once
+        sum = sum & 0x0000FFFF; 
+        sum = cksum + sum;
+
+        cksum = sum >> 16;                  // Fold once more
+        sum = sum & 0x0000FFFF;
+        cksum = cksum + sum;
+        cksum = 0xFFFF^cksum;
+
+        t.checksum = cksum;
+        run++;                      // Goes to the next run of the server
+
+        buffer[2] = t.sequence;
+        buffer[3] = t.ack;
+        buffer[4] = s_bit;
+        buffer[5] = a_bit;
+        buffer[7] = t.checksum;
+
+        // PRINTING
+        printf("SERVER SENDING TO CLIENT\n");
+        printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+        printf("Seq: %d\n", t.sequence);
+        printf("ACK: %d\n", t.ack);
+        printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+        printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+        printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d", s_bit, f_bit, t.window);
+        printf("CHECKSUM: 0x%04X\n", t.checksum);
+        printf("OPTIONS: %d\n DATA: 0\n", t.options);
+        printf("\n\n");
+
+        n = write(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR on writing to socket");
+
     }
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-    	error("ERROR on binding");
+    if(run == 2)
+    {
+        n = read(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR reading to socket");
 
-    while(1) {
-    	listen(sockfd, 5);
-    	clilen = sizeof(cli_addr);
-    	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    	if(newsockfd < 0) 
-    		error("ERROR on accept");
-    	bzero(buffer, 256);
+        t.sequence = buffer[2];     // Sequence Number
+        t.ack = t.sequence + 1;     // Ack number
+            // FIN AND ACK BITS
+        a_bit = 1;                  // ACKBIT
 
-    	n = read(newsockfd, buffer, 255);
-    	if(n < 0) error("ERROR on read");
-
-    	if(buffer[0] == 'b' && buffer[1] == 'u' && buffer[2] == 'y') {
-    		for(i = 0; i < 10; i++) {
-    			if(t_sold[i] == 0) {
-    				t_sold[i] = 1;	// Assigns 1 meaing its sold
-    	// Converts the int ticket number to characters and puts it in buffer
-    				snprintf(buffer, 10, "%d", ticket[i]);
-    				printf("Sold: %s\n", buffer);
-    				t_count++;// Counter to keep track of tickets sold
-    				goto t_sold;
-    			}
-    			else if(t_count == 10) {
-    				n = write(newsockfd, "No more tickets available", 25);
-    				if(n < 0) error("ERROR on write");
-    				goto end;
-    			}
-    		}
-    		t_sold:;
-    		n = write(newsockfd, buffer, 255);
-    	}
-    	end:;
-
-    	if(buffer[0] == 'c' && buffer[1] == 'a' && buffer[2] == 'n' && buffer[3] == 'c' && buffer[4] == 'e' && buffer[5] == 'l') {  		
-    		n = read(newsockfd, buffer, 255);
-    		if(n < 0) error("ERROR reading from socket");
-			to_cancel = atoi(buffer);        // Converts character buffer to int
-    		for(i = 0; i < 10; i++) {
-    			if(ticket[i] == to_cancel) {
-    				t_sold[i] = 0;			// Sets location to not sold
-					printf("Ticket %d cancelled", to_cancel);
-    			}
-    		}
-    	}
-    	bzero(buffer, 256);
-    	close(newsockfd);
+        // PRINTING
+        printf("FINAL RESPONSE FROM CLIENT\n");
+        printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+        printf("Seq: %d\n", t.sequence);
+        printf("ACK: %d\n", t.ack);
+        printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+        printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+        printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d", s_bit, f_bit, t.window);
+        printf("CHECKSUM: 0x%04X\n", t.checksum);
+        printf("OPTIONS: %d\n DATA: 0\n", t.options);
+        printf("\n\n");
+        run++;
     }
-    close(sockfd);
+
+/* CLOSING TCP CONNECTION */
+    printf("\n\nCLOSING TCP CONNECTION\n");
+    if(run == 3)
+    {
+        n = read(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR reading to socket");  
+
+        srand(time(NULL));                   // Randomize the random seed
+        t.sequence = random() % BUFFSIZE;
+        t.ack = buffer[2] + 1;
+        a_bit = 1;
+
+        buffer[1] = t.sequence;
+        buffer[2] = t.ack;
+        buffer[5] = a_bit;
+
+        // Runs Checksum
+        memcpy(cksum_arr, &t, 24);          //Copying 24 bytes
+        for (i=0;i<12;i++)                  // Compute sum
+            sum = sum + cksum_arr[i];
+
+        cksum = sum >> 16;                  // Fold once
+        sum = sum & 0x0000FFFF; 
+        sum = cksum + sum;
+
+        cksum = sum >> 16;                  // Fold once more
+        sum = sum & 0x0000FFFF;
+        cksum = cksum + sum;
+        cksum = 0xFFFF^cksum;
+
+        t.checksum = cksum;
+        run++;                      // Goes to the next run of the server
+
+        // PRINTING
+        printf("SERVER SENDING TO CLIENT\n");
+        printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+        printf("Seq: %d\n", t.sequence);
+        printf("ACK: %d\n", t.ack);
+        printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+        printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+        printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d", s_bit, f_bit, t.window);
+        printf("CHECKSUM: 0x%04X\n", t.checksum);
+        printf("OPTIONS: %d\n DATA: 0\n", t.options);
+        printf("\n\n");
+
+        n = write(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR on writing to socket");
+
+        srand(time(NULL));                   // Randomize the random seed
+        t.sequence = random() % BUFFSIZE;
+        t.ack = buffer[2] + 1;
+        f_bit = 1;
+
+        // Runs Checksum
+        memcpy(cksum_arr, &t, 24);          //Copying 24 bytes
+        for (i=0;i<12;i++)                  // Compute sum
+            sum = sum + cksum_arr[i];
+
+        cksum = sum >> 16;                  // Fold once
+        sum = sum & 0x0000FFFF; 
+        sum = cksum + sum;
+
+        cksum = sum >> 16;                  // Fold once more
+        sum = sum & 0x0000FFFF;
+        cksum = cksum + sum;
+        cksum = 0xFFFF^cksum;
+
+        t.checksum = cksum;
+
+        buffer[2] = t.sequence;
+        buffer[4] = f_bit;
+        buffer[5] = a_bit;
+
+        n = write(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR on writing to socket");
+
+
+        // PRINTING
+        printf("\nSECOND CLOSE ACK\n");
+        printf("SERVER SENDING TO CLIENT\n");
+        printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+        printf("Seq: %d\n", t.sequence);
+        printf("ACK: %d\n", t.ack);
+        printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+        printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+        printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d", s_bit, f_bit, t.window);
+        printf("CHECKSUM: 0x%04X\n", t.checksum);
+        printf("OPTIONS: %d\n DATA: 0\n", t.options);
+        printf("\n\n");
+
+    }
+    if(run == 4)
+    {
+        n = read(newSocket, buffer, sizeof(buffer));
+        if(n < 0) error("ERROR reading to socket"); 
+
+        t.sequence = buffer[2];
+        t.ack = buffer[3];
+        a_bit = buffer[5];
+
+
+
+        // PRINTING
+        printf("\nFINAL CLOSE ACK\n");
+        printf("SERVER RECIEVING FROM CLIENT\n");
+        printf("Source: %d, Dest: %d\n", t.sourcePort, t.destPort); // Printing all values
+        printf("Seq: %d\n", t.sequence);
+        printf("ACK: %d\n", t.ack);
+        printf("DATA OFFSET: 5\n\tRESERV: 0 \n\tURGFLAG: 0 \n");
+        printf("\tACKFLAG: %d\n\tPSH: 0 \n\tRST: 0 \n", a_bit);
+        printf("\tSYN: %d \n\tFINBIT: %d \n\tWINDOW: %d", s_bit, f_bit, t.window);
+        printf("CHECKSUM: 0x%04X\n", t.checksum);
+        printf("OPTIONS: %d\n DATA: 0\n", t.options);
+        printf("\n\n");
+
+    }
+
+
+    // Closes all sockets creates
+    close(createSocket);
+    close(newSocket);
+    // Releases all the memory used
+    free(buffer);
+    free(buffer2);
     return 0;
 }
